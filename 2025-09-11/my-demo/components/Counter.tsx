69c0b729d66fb8ad8de9ee76c0fcb2b967ef98ef
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-09-16 17:10:08 +0300

    feat: buttons to modify click counter

--- 2025-09-11/my-demo/components/Counter.tsx
+++ 2025-09-11/my-demo/components/Counter.tsx
@@ -3,16 +3,48 @@ import { useState } from "react"
 function Counter() {
   const [count, setCount] = useState(0)
 
-  function increaseCounter(amount: number){
+  function updateCounter(amount: number){
     setCount(count => count + amount)
   }
   return (
     <>
         <h1>Vite you + React me + and her</h1>
         <div className="card">
-          <button onClick={() => increaseCounter(10)}>
-              count is {count}
+
+          <div>{count}</div>
+
+          <button onClick={() => updateCounter(100)}>
+              +100
           </button>
+
+          <button onClick={() => updateCounter(50)}>
+              +50
+          </button>
+          
+          <button onClick={() => updateCounter(25)}>
+              +25
+          </button>
+
+          <button onClick={() => updateCounter(1)}>
+              +1
+          </button>
+
+          <button onClick={() => updateCounter(-1)}>
+              -1
+          </button>
+          
+          <button onClick={() => updateCounter(-25)}>
+              -25
+          </button>
+          
+          <button onClick={() => updateCounter(-50)}>
+              -50
+          </button>
+
+          <button onClick={() => updateCounter(-100)}>
+              -100
+          </button>
+          
         </div>
     </>
   )
