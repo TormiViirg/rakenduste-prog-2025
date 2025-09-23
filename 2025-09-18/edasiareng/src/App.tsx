329bc0ed8f0d5e8d5e5c6f08ca01bb2910107f23
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-09-23 18:21:05 +0300

    chore: initialization finished

--- 2025-09-18/edasiareng/src/App.tsx
+++ 2025-09-18/edasiareng/src/App.tsx
@@ -1,3 +1,10 @@
+import { Link, Outlet, Route, Routes } from 'react-router'
+
+import Home from "../components/Home"
+import AboutMe from "../components/AboutMe";
+import Portfolio from "../components/Portfolio";
+import LocalStorage from "../components/LocalStorage";
+
 import './App.css'
 import Button from "@mui/material/Button"
 
@@ -5,11 +12,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <Button variant="contained">Hello MUI</Button>
-        <Button variant="outlined">Outlined</Button>
-        <Button variant="text">Text</Button>
-      </div>
+      <nav>
+        <Button>
+          <Link to="/">Home</Link>
+        </Button>
+        <Button>
+          <Link to="/localStorage">Local Storage</Link>
+        </Button>
+        <Button>
+          <Link to="/portfolio">Portfolio</Link>
+        </Button>
+        <Button>
+          <Link to="/aboutMe">About Me</Link>
+        </Button>
+      </nav>
+      <Outlet/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/localstorage" element={<LocalStorage/>}/>
+        <Route path="/portfolio" element={<Portfolio/>}/>
+        <Route path="/aboutMe" element={<AboutMe/>}/>
+      </Routes>
     </>
   )
 }
--- 2025-09-18/edasiareng/src/main.tsx
+++ 2025-09-18/edasiareng/src/main.tsx
@@ -1,18 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createHashRouter } from "react-router";
+import { HashRouter } from 'react-router';
 import './index.css'
 import App from './App.tsx'
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <HashRouter>
+    <App/>
+  </HashRouter>
 )
