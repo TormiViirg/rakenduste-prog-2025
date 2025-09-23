d4a7f350d206111cd9a61bbc6a4eee5dcb05b625
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-09-23 23:28:32 +0300

    feat: basic crud functionality

--- 2025-09-18/backend/index.js
+++ 2025-09-18/backend/index.js
@@ -3,9 +3,25 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("helloWorld");
+    res.send("Chalk marks under the bridge");
 });
 
-app.listen(port, () => {
-    console.log("ertyukl")
-});
+app.post("/", (req, res) => {
+    res.send("A new rock in the park, under the willow tree")
+})
+
+app.put("/", (req, res) => {
+    res.send("The codes no longer work")
+})
+
+app.delete("/", (req, res) => {
+    res.send("I have been made reduntant")
+})
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send(req.params)
+})
+
+app.get('/flights/:from-:to', (req, res) => {
+  res.send(req.params)
+})
