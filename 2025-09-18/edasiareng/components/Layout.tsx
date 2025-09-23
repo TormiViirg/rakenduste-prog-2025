581efb81086c936c7e75e8012ebeae73a8a6eeee
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-09-23 19:57:49 +0300

    feature drawer menu mui

--- 2025-09-18/edasiareng/components/Layout.tsx
+++ 2025-09-18/edasiareng/components/Layout.tsx
@@ -1,26 +1,59 @@
 import { Outlet, Link } from "react-router";
-import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+
+const drawerWidth = 200;
+const navItems = [
+  { label: "Home", path: "/" },
+  { label: "Portfolio", path: "/portfolio" },
+  { label: "About Me", path: "/aboutMe" },
+  { label: "Local Storage", path: "/localStorage" },
+];
 
 export default function Layout() {
   return (
-    <div>
-      <nav>
-        <Button>
-          <Link to="/">Home</Link>
-        </Button>
-        <Button>
-          <Link to="/localStorage">Local Storage</Link>
-        </Button>
-        <Button>
-          <Link to="/portfolio">Portfolio</Link>
-        </Button>
-        <Button>
-          <Link to="/aboutMe">About Me</Link>
-        </Button>
-      </nav>
+    <Box sx={{ display: "flex" }}>
+      {/* Drawer */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6">My Portfolio</Typography>
+        </Toolbar>
+        <List>
+          {navItems.map((item) => (
+            <ListItem key={item.label} disablePadding>
+              <ListItemButton component={Link} to={item.path}>
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
 
-      <Outlet />
-
-    </div>
+      {/* Main content */}
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Toolbar /> {/* Spacer for AppBar */}
+        <Outlet />
+      </Box>
+    </Box>
   );
 }
