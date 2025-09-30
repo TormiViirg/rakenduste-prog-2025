b78b9ea5280cd27a7a3a13362cce349db8102791
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-09-30 18:29:14 +0300

    feat:simple backend example, crud routes, middleware, controllers and express initialization

--- 2025-09-25/tormi/backend/index.js
+++ 2025-09-25/tormi/backend/index.js
@@ -1,1 +1,19 @@
-app.use
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+const catsRoutes = require("./routes/cats.routes");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/cats", catsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ 2025-09-25/tormi/backend/routes/cats.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const catsController = require("../controllers/cats.controller");
+const {
+  catsRouteMiddleware,
+  catsGetRouteMiddleware,
+} = require("../middlewares/cats.middlewares");
+
+router.use(catsRouteMiddleware);
+
+router.get("/", catsGetRouteMiddleware, catsController.read);
+router.post("/", catsController.create);
+router.put("/", catsController.update);
+router.delete("/", catsController.delete);
+
+module.exports = router;
