8a635be9018a71462d796a7575f6291cc3e63ddb
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-09-30 21:30:08 +0300

    feat: added validation

--- 2025-09-25/tormi/backend/routes/cats.routes.js
+++ 2025-09-25/tormi/backend/routes/cats.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body, param } = require("express-validator");
 const router = express.Router();
 const catsController = require("../controllers/cats.controller");
 const {
@@ -9,8 +10,23 @@ const {
 router.use(catsRouteMiddleware);
 
 router.get("/", catsGetRouteMiddleware, catsController.read);
-router.post("/", catsController.create);
+/*router.post("/", catsController.create);
 router.put("/", catsController.update);
-router.delete("/", catsController.delete);
+router.delete("/", catsController.delete);*/
+
+router.post(
+  "/",
+  body("name").isString().trim().isLength({ min: 1, max: 50 }),
+  catsController.create
+);
+
+router.put(
+  "/:id",
+  param("id").isUUID(),
+  body("name").optional().isString().trim().isLength({ min: 1, max: 50 }),
+  catsController.update
+);
+
+router.delete("/:id", param("id").isUUID(), catsController.delete);
 
 module.exports = router;
